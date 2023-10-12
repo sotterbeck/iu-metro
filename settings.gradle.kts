8e4c9886d413c6b93fca4ce7a64c9c359bf7dbf6
Simon Otterbeck <simon.otterbeck@protonmail.com>
2023-10-13 00:36:14 +0200

    build: update settings.gradle to refer to postgres project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,9 +2,8 @@ rootProject.name = "iu-metro"
 include(
     "entity",
     "usecase",
-    "entry",
     "entrypoints:papermc",
-    "dataproviders:jpa"
+    "dataproviders:postgres"
 )
-findProject(":entrypoints:papermc")?.name = "papermc"
-findProject(":dataproviders:jpa")?.name = "jpa"
+findProject(":entrypoints:papermc")
+findProject(":dataproviders:postgres")
