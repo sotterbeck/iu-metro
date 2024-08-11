810767d73cef5d2ad037dc53a6197de272e81daa
Simon Otterbeck <simon.otterbeck@protonmail.com>
2024-08-12 00:01:58 +0200

    feat: show debug info for admins when a fare gate sign is left-clicked

--- entrypoints/papermc/src/main/java/de/sotterbeck/iumetro/entrypoint/papermc/faregate/sign/FareGateSignClickHandler.java
+++ entrypoints/papermc/src/main/java/de/sotterbeck/iumetro/entrypoint/papermc/faregate/sign/FareGateSignClickHandler.java
@@ -1,19 +1,25 @@
 package de.sotterbeck.iumetro.entrypoint.papermc.faregate.sign;
 
+import de.sotterbeck.iumetro.entrypoint.papermc.common.Components;
 import de.sotterbeck.iumetro.entrypoint.papermc.common.sign.SignClickEvent;
 import de.sotterbeck.iumetro.entrypoint.papermc.common.sign.SignClickHandler;
+import de.sotterbeck.iumetro.entrypoint.papermc.faregate.FareGateKeyFactory;
 import de.sotterbeck.iumetro.usecase.faregate.FareGateControlInteractor;
 import de.sotterbeck.iumetro.usecase.faregate.FareGateControlRequestModel;
 import de.sotterbeck.iumetro.usecase.faregate.PositionDto;
 import org.bukkit.block.Sign;
 import org.bukkit.block.data.type.WallSign;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
 
 public class FareGateSignClickHandler implements SignClickHandler {
 
     private final FareGateControlInteractor fareGateControlInteractor;
+    private final FareGateKeyFactory fareGateKeyFactory;
 
-    public FareGateSignClickHandler(FareGateControlInteractor fareGateControlInteractor) {
+    public FareGateSignClickHandler(FareGateControlInteractor fareGateControlInteractor, FareGateKeyFactory fareGateKeyFactory) {
         this.fareGateControlInteractor = fareGateControlInteractor;
+        this.fareGateKeyFactory = fareGateKeyFactory;
     }
 
     @Override
@@ -28,6 +34,22 @@ public class FareGateSignClickHandler implements SignClickHandler {
         FareGateControlRequestModel request = new FareGateControlRequestModel(position, orientation);
 
         fareGateControlInteractor.openGate(request);
+    }
+
+    @Override
+    public void onLeftClick(SignClickEvent event) {
+        if (!event.player().hasPermission("iumetro.admin")) {
+            return;
+        }
+        Sign sign = event.sign();
+        PersistentDataContainer container = sign.getPersistentDataContainer();
+
+        String type = container.get(fareGateKeyFactory.getFareGateTypeKey(), PersistentDataType.STRING);
+        String stationId = container.get(fareGateKeyFactory.getStationKey(), PersistentDataType.STRING);
+
+        String debugInfo = "<gold><bold>[DEBUG]</bold><yellow> type: <white>%s <yellow>stationId: <white>%s".formatted(type, stationId);
+
+        event.player().sendActionBar(Components.mm(debugInfo));
     }
 
 }
